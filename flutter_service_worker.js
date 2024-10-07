47dde476cd81a156993c2536be3ad7bb55069ed8
WaheedHussainHaans <WaheedHussainHaans@users.noreply.github.com>
2024-10-07 15:38:52 +0000

    deploy: e9cf48370da289809c6be9d6d57a65352054bc2a

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -102,11 +102,11 @@ const RESOURCES = {"assets/AssetManifest.json": "572e06e6b5911f3c4feef0787730d0c
 "icons/Icon-192.png": "002516a4b23f153b7ac2928347c377b1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
-"flutter_bootstrap.js": "9c35eb2c43ecce2403942682f0b0c4af",
+"flutter_bootstrap.js": "65d61bc416a092dfdb1326ed107b10cf",
 "favicon.png": "5bb9eebc8c69798b4806ffd7ef708350",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"index.html": "1a380c8458cc8f68024ea80ede60bfe3",
-"/": "1a380c8458cc8f68024ea80ede60bfe3",
+"index.html": "6bf1f2c24de33ec2818bf41b16419fa5",
+"/": "6bf1f2c24de33ec2818bf41b16419fa5",
 "version.json": "a29e8b22a08aa0274b027931eec216c4",
 "manifest.json": "679580f20353c2770bf78c1344d1b69d"};
 // The application shell files that are downloaded before a service worker can
